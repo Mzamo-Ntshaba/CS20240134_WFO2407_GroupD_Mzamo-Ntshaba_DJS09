7e1af691e37e1fed70f75181c57aab273bba9169
Mzamo Ntshaba <mzamontshaba14@gmail.com>
2024-11-21 23:35:23 +0200

    Added Typescript core components and utilities

---
+++ src/interface.ts
@@ -0,0 +1,27 @@
+import { Country, Price} from './types'
+enum LoyaltyUser {
+    GOLD_USER = 'GOLD_USER',
+    SILVER_USER = 'SILVER_USER',
+    BRONZE_USER = 'BRONZE_USER'
+}
+
+export interface Review {
+    name: string;
+    stars: number;
+    loyaltyUser: LoyaltyUser;
+    date: string;
+}
+
+export interface Property {
+    image: string;
+    title: string;
+    price: Price;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number | string;
+        country: Country
+    }
+    contact: [ number, string];
+    isAvailable: boolean;
+}
---
+++ src/utils.ts
@@ -0,0 +1,57 @@
+
+import { Review } from './interface'
+/* showReviewTota Function to display review total and reviewer information
+   Determine whether the user is a gold user (loyalty status)
+   Update the reviewTotalDisplay element's content
+   populateUser Function to populate user-related information
+   If the user is returning, display "back" in the returningUserDisplay element
+   display the user's name in the userNameDisplay element
+   show details funtion
+   make multiple fuction with type of return
+   */
+
+const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
+const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
+const userNameDisplay = document.querySelector('#user') as HTMLElement
+
+enum Permissions {
+    ADMIN = 'ADMIN', 
+    READ_ONLY = 'READ_ONLY'
+}
+
+enum LoyaltyUser {
+    GOLD_USER = 'GOLD_USER',
+    SILVER_USER = 'SILVER_USER',
+    BRONZE_USER = 'BRONZE_USER'
+}
+
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = value.toString() + ' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay    
+}
+
+export function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
+
+export function makeMultiple(value: number) : string {
+    if (value > 1 || value == 0) {
+        return 's'
+    } else return ''
+}
+
+export function getTopTwoReviews(reviews : Review[]) : Review[]  {
+    const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
+    return sortedReviews.slice(0,2)
+   }
